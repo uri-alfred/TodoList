bb551941aaef525d47abb4d235e50bd3389d1702
zzALFREDzz\Uriel <urielbotelloresendiz@gmail.com>
2024-02-22 20:29:39 -0600

    jueves parte 1

---
+++ TodoList/ViewModels/TodoviewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using TodoList.Models;
+using TodoList.Pages;
+using TodoList.Services;
+
+namespace TodoList.ViewModels
+{
+    public partial class TodoviewModel : ObservableObject
+    {
+        public ObservableCollection<Tarea> Tareas { get; set; }
+        private FakeTaskService fakeService;
+
+        [ObservableProperty]
+        private Tarea tareaSeleccionada;
+
+        [ObservableProperty]
+        private bool isRefresh;
+
+        public TodoviewModel(FakeTaskService service)
+        {
+            Tareas = new ();
+            fakeService = service;
+        }
+
+        [RelayCommand]
+        public void AgregarTareas()
+        {
+            IsRefresh = true;
+            RefreshItems();
+            IsRefresh = false;
+        }
+
+        void RefreshItems()
+        {
+            Tareas.Clear();
+            fakeService.Tasks.ForEach(Tareas.Add);
+        }
+
+        [RelayCommand]
+        public void AbrirRegistro()
+        {
+            Shell.Current.GoToAsync(nameof(RegistroTareaPage));
+        }
+
+        [RelayCommand]
+        public void EditarRegistro()
+        {
+            if (TareaSeleccionada == null)
+            {
+                return;
+            }
+
+            ShellNavigationQueryParameters parametros = new()
+            {
+                { "TAREA", TareaSeleccionada }
+            };
+
+            Shell.Current.GoToAsync(nameof(RegistroTareaPage), parametros);
+        }
+    }
+}
